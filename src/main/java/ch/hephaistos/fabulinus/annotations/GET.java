30f9739330829f4f208265e33076880093393272
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-08 22:10:14 +0200

    Creates GET annotation
    for #2

---
+++ src/main/java/ch/hephaistos/fabulinus/annotations/GET.java
@@ -0,0 +1,23 @@
+package ch.hephaistos.fabulinus.annotations;
+
+
+import java.lang.annotation.*;
+
+/**
+ * This Annotation creates a way to access the annoted variable trough a HTTP REST API.
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface GET {
+
+    /**
+     * This variable holds the name for the target function. If you want
+     * to use your own function, that is <b>not</b> called <b>getVariableName</b> then define it with
+     * this variable
+     * @return
+     */
+    public String function () default "";
+
+}
