8ff35188a2cb35896e7679a92f2892d1f9e739ae
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 21:16:48 +0200

    Adds functionality to get value trough reflection
    For this useCase it uses a 2nd constructor that takes in a Field instead of a Method

--- src/main/java/ch/hephaistos/fabulinus/annotations/GetValueAdapter.java
+++ src/main/java/ch/hephaistos/fabulinus/annotations/GetValueAdapter.java
@@ -2,25 +2,39 @@ package ch.hephaistos.fabulinus.annotations;
 
 import ch.hephaistos.fabulinus.adapter.ValueAdapter;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class GetValueAdapter implements ValueAdapter {
 
     private Object object;
-
+    
     private Method method;
 
+    private Field field;
 
     public GetValueAdapter(Object object, Method method){
         this.object = object;
         this.method = method;
     }
 
+    public GetValueAdapter(Object object, Field field){
+        this.object = object;
+        this.field = field;
+    }
+
     @Override
     public Object invokeFunction() {
         try {
-            return method.invoke(object);
+            if(method == null){
+                field.setAccessible(true);
+                Object returnObject =  field.get(object);
+                field.setAccessible(false);
+                return returnObject;
+            } else {
+                return method.invoke(object);
+            }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
