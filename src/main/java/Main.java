18b05b9ba1a9b48c1195e4cd77a071c8cad6856a
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 21:17:20 +0200

    More example code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,15 @@ import ch.hephaistos.fabulinus.formatter.XmlDataFormatter;
 public class Main {
 
     public static void main(String args[]) {
-        ExampleObject exampleObject = new ExampleObject("newNameWhoThis?", 27);
+        ExampleObject exampleObject = new ExampleObject("newNameWhoThis?", 27, 27.32);
         RequestHandler requestHandler = new Fabulinus()
                 .setDataFormatter(new JsonDataFormatter())
                 .setupFabulinus(exampleObject)
                 .create();
         System.out.println(requestHandler.getValue("name"));
         System.out.println(requestHandler.getValue("age"));
+        System.out.println(requestHandler.getValue("number"));
+        System.out.println(requestHandler.getValue("unreachableString"));
 
         requestHandler = new Fabulinus()
                 .setDataFormatter(new XmlDataFormatter())
@@ -20,6 +22,8 @@ public class Main {
                 .create();
         System.out.println(requestHandler.getValue("name"));
         System.out.println(requestHandler.getValue("age"));
+        System.out.println(requestHandler.getValue("number"));
+        System.out.println(requestHandler.getValue("unreachableString"));
     }
 
 }
