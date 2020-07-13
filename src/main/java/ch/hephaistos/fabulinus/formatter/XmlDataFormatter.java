d093e41c40a284c07c5d542dcc6a4f0bd9747c7e
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 20:29:41 +0200

    Adds very basic XmlFormatter

---
+++ src/main/java/ch/hephaistos/fabulinus/formatter/XmlDataFormatter.java
@@ -0,0 +1,11 @@
+package ch.hephaistos.fabulinus.formatter;
+
+
+public class XmlDataFormatter implements DataFormatter {
+
+    @Override
+    public String formatData(String variableName, String value) {
+        return "<".concat(variableName).concat("=\"").concat(value).concat("\"/>");
+    }
+}
+
