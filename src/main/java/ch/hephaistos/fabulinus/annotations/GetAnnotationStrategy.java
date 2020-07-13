33bdf4963c4e27bf8436f715219b719e4e4c7538
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 21:16:06 +0200

    Adds functionality for all 3 cases of annotation usage
    1. Allows the user to define a getVariableName function, finds it and pairs it
    2. Allows the user to define a specific function, finds it and pairs it
    3. creates a get functionality trough reflection if no function is present

--- src/main/java/ch/hephaistos/fabulinus/annotations/GetAnnotationStrategy.java
+++ src/main/java/ch/hephaistos/fabulinus/annotations/GetAnnotationStrategy.java
@@ -18,23 +18,41 @@ public class GetAnnotationStrategy implements AnnotationStrategy {
 
         List<Pair<String, ValueAdapter>> pairs = new ArrayList();
         Class clazz = object.getClass();
-        for (Field field : clazz.getDeclaredFields()) {
-            if (field.isAnnotationPresent(GET.class)) {
-                if (field.getAnnotation(GET.class).function().isEmpty()) {
-                    pairs.add(linkAnonymousFunction(field, object, clazz));
-                }
-            }
-        }
+        Arrays.asList(clazz.getDeclaredFields()).stream()
+                .filter(field -> field.isAnnotationPresent(GET.class))
+                .forEach(field -> {
+                    String functionName = generateFunctionName(field.getName());
+                    if (field.getAnnotation(GET.class).function().isEmpty()) {
+                        if (Arrays.asList(clazz.getDeclaredMethods()).stream().anyMatch(functionName::equals)) {
+                            pairs.add(linkFunction(functionName, field, object, clazz));
+                        } else {
+                            pairs.add(linkAnonymousFunction(field, object));
+                        }
+                    } else {
+                        pairs.add(linkFunction(field.getAnnotation(GET.class).function(), field, object, clazz));
+                    }
+                });
         return pairs;
     }
 
-    private Pair<String, ValueAdapter> linkAnonymousFunction(Field field, Object object, Class clazz) {
-        String functionName = "get".concat(field.getName().substring(0, 1).toUpperCase()).concat(field.getName().substring(1));
-        Method method = Arrays.asList(clazz.getDeclaredMethods()).stream().filter(m -> m.getName().equals(functionName)).findFirst().get();
+    private Pair<String, ValueAdapter> linkFunction(String functionName, Field field, Object object, Class clazz) {
+        Method method = getMethodForFunctionName(functionName, clazz);
         if (method.getReturnType() != field.getType()) {
             throw new RuntimeException("Function doesn't return the correct type!");
         }
         return new Pair(field.getName(), new GetValueAdapter(object, method));
+    }
+
+    private Pair<String, ValueAdapter> linkAnonymousFunction(Field field, Object object) {
+        return new Pair(field.getName(), new GetValueAdapter(object, field));
+    }
+
+    private Method getMethodForFunctionName(String functionName, Class clazz) {
+        return Arrays.asList(clazz.getDeclaredMethods()).stream().filter(m -> m.getName().equals(functionName)).findFirst().get();
+    }
+
+    private String generateFunctionName(String fieldName) {
+        return "get".concat(fieldName.substring(0, 1).toUpperCase()).concat(fieldName.substring(1));
     }
 
 }
