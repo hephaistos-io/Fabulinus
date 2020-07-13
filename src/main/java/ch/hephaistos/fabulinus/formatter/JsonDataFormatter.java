f132880d4644877e1b63352db2d96e81c935bfd8
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 20:32:05 +0200

    Removes gson dependency
    changes Json data formatter to be a very basic formatter

--- src/main/java/ch/hephaistos/fabulinus/formatter/JsonDataFormatter.java
+++ src/main/java/ch/hephaistos/fabulinus/formatter/JsonDataFormatter.java
@@ -1,14 +1,10 @@
 package ch.hephaistos.fabulinus.formatter;
 
-import com.google.gson.JsonObject;
-
 public class JsonDataFormatter implements DataFormatter {
 
     @Override
     public String formatData(String variableName, String value) {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty(variableName, value);
-        return jsonObject.toString();
+        return "{".concat("\"").concat(variableName).concat("\":\"").concat(value).concat("\"}");
     }
 }
 
