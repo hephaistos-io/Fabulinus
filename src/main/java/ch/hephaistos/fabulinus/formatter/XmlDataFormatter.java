592ef30f94fa071088c4ee4337e7a31b47254a03
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 21:16:58 +0200

    Changed formatting of Xml output

--- src/main/java/ch/hephaistos/fabulinus/formatter/XmlDataFormatter.java
+++ src/main/java/ch/hephaistos/fabulinus/formatter/XmlDataFormatter.java
@@ -5,7 +5,7 @@ public class XmlDataFormatter implements DataFormatter {
 
     @Override
     public String formatData(String variableName, String value) {
-        return "<".concat(variableName).concat("=\"").concat(value).concat("\"/>");
+        return "<".concat(variableName).concat(">").concat(value).concat("<").concat(variableName).concat("/>");
     }
 }
 
