98b8f93d298242796ed487973b3cbdb3b9fae02e
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 20:32:13 +0200

    Adds example code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
 import ch.hephaistos.fabulinus.adapter.Fabulinus;
 import ch.hephaistos.fabulinus.adapter.RequestHandler;
+import ch.hephaistos.fabulinus.formatter.JsonDataFormatter;
+import ch.hephaistos.fabulinus.formatter.XmlDataFormatter;
 
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         ExampleObject exampleObject = new ExampleObject("newNameWhoThis?", 27);
-        RequestHandler requestHandler = Fabulinus.setupFabulinus(exampleObject);
+        RequestHandler requestHandler = new Fabulinus()
+                .setDataFormatter(new JsonDataFormatter())
+                .setupFabulinus(exampleObject)
+                .create();
+        System.out.println(requestHandler.getValue("name"));
+        System.out.println(requestHandler.getValue("age"));
+
+        requestHandler = new Fabulinus()
+                .setDataFormatter(new XmlDataFormatter())
+                .setupFabulinus(exampleObject)
+                .create();
         System.out.println(requestHandler.getValue("name"));
         System.out.println(requestHandler.getValue("age"));
     }
