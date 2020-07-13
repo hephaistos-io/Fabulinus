4875bf8d5c0497879fb651527e6a5854e3618eb9
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-13 20:27:38 +0200

    Refactors into some sort of builder

--- src/main/java/ch/hephaistos/fabulinus/adapter/Fabulinus.java
+++ src/main/java/ch/hephaistos/fabulinus/adapter/Fabulinus.java
@@ -1,12 +1,27 @@
 package ch.hephaistos.fabulinus.adapter;
 
 import ch.hephaistos.fabulinus.annotations.GetAnnotationStrategy;
+import ch.hephaistos.fabulinus.formatter.DataFormatter;
 
 public class Fabulinus {
 
-    public static RequestHandler setupFabulinus(Object object) {
-        RequestHandler requestHandler = new RequestHandler();
+    private RequestHandler requestHandler;
+
+    public Fabulinus(){
+        requestHandler = new RequestHandler();
+    }
+
+    public Fabulinus setupFabulinus(Object object) {
         requestHandler.addEntries(new GetAnnotationStrategy().parseFields(object));
+        return this;
+    }
+
+    public Fabulinus setDataFormatter(DataFormatter dataFormatter){
+        requestHandler.setDataFormatter(dataFormatter);
+        return this;
+    }
+
+    public RequestHandler create(){
         return requestHandler;
     }
 }
