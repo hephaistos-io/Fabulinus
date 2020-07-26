3b2580bafb95cb27b68c82aeb6c8970a9af8a7f5
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-07-26 20:55:21 +0200

    Enhances example object with POST annotation

--- src/main/java/ExampleObject.java
+++ src/main/java/ExampleObject.java
@@ -1,24 +1,44 @@
 import ch.hephaistos.fabulinus.annotations.GET;
+import ch.hephaistos.fabulinus.annotations.NotNullPostValidatorStrategy;
+import ch.hephaistos.fabulinus.annotations.POST;
 
 public class ExampleObject {
 
     @GET
+    @POST(validator = NotNullPostValidatorStrategy.class)
     private String name;
 
     @GET
+    @POST
     private int age;
 
     @GET
+    @POST
     private double number;
 
     @GET(function="randomString")
+    @POST(function="randomString")
     private String unreachableString;
+
+    private final String RANDOM_STRING = "asidjkslamdlkasdop";
 
     public ExampleObject(String name, int age, double number){
         this.name = name;
         this.age = age;
         this.number = number;
         unreachableString = "haha can't get me!";
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setRandomString(){
+        this.name = RANDOM_STRING;
     }
 
     public String getName(){
