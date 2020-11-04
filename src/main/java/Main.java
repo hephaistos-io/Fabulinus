0d8ccc18a29ea76ab1ecf36ddea0042919543592
Ricardo Monteiro Simoes <ricardo_simoes_93@hotmail.com>
2020-11-04 17:56:13 +0100

    Adds more testing statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import ch.hephaistos.fabulinus.formatter.JsonDataFormatter;
 public class Main {
 
     public static void main(String args[]) {
-        ExampleObject exampleObject = new ExampleObject("newNameWhoThis?", 27, 27.32, 77f, 1337l);
+        ExampleObject exampleObject = new ExampleObject("newNameWhoThis?", 27, 27.32, 77f, true, 1337l);
         RequestHandler requestHandler = new Fabulinus()
                 .setDataFormatter(new JsonDataFormatter())
                 .setupFabulinus(exampleObject)
@@ -15,12 +15,20 @@ public class Main {
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "age"));
         System.out.println("Was able to POST value?:" + requestHandler.applyStoringRequest(RequestType.POST, "age", "12"));
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "age"));
+        System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "bool"));
+        System.out.println("Was able to POST value?:" + requestHandler.applyStoringRequest(RequestType.POST, "bool", "false"));
+        System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "bool"));
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "floatingNumber"));
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "longBoy"));
         System.out.println("Was able to POST value?:" + requestHandler.applyStoringRequest(RequestType.POST, "floatingNumber", "27f"));
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "floatingNumber"));
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "number"));
         System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "unreachableString"));
+
+
+        System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "number"));
+        System.out.println("Was able to POST value?:" + requestHandler.applyStoringRequest(RequestType.POST, "number", "12.33"));
+        System.out.println(requestHandler.applyRetrievalRequest(RequestType.GET, "number"));
 
     }
 
